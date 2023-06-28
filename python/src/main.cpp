7347680a536152b57c1fe76ad556c45544f4d77c
wonjun-16 <wlee10@g.ucla.edu>
2023-06-28 08:25:10 -0500

    fixed errors

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -26,6 +26,7 @@ void function1(py::array_t<double>& f_np, py::array_t<double>& X_np){
 }
 
 class HelperClass{
+public:
 // variatbles
     int n1;
     int n2;
@@ -38,7 +39,6 @@ class HelperClass{
 
     HelperClass(py::array_t<double>& phi_np, const double dx, const double dy){
         py::buffer_info phi_buf = phi_np.request();
-        double* phi = static_cast<double *>(phi_buf.ptr);
         this->n1 = phi_buf.shape[0];
         this->n2 = phi_buf.shape[1];
 
@@ -51,7 +51,7 @@ class HelperClass{
     }
 
     // This function will provide S1(x,) where x and y are n [0,1] double values
-    double interpolate_function(double x,double y,const double* func) const{
+    double interpolate_function(double x,double y, std::vector<double>& func) const{
         double indj=fmin(n1-1,fmax(0,x*n1-0.5));
         double indi=fmin(n2-1,fmax(0,y*n2-0.5));
 
@@ -69,38 +69,38 @@ class HelperClass{
         return interpolated_value;  
     }
 
-    void calculate_gradient_vxx(double* vxx, const double* phi){
+    void calculate_gradient_vxx(std::vector<double>& vxx, const double* phi, const double dx){
         for(int i=0;i<n2;++i){
             for(int j=0;j<n1;++j){
                 int jpp = fmin(n1-1,j+2);
-                int jp  = fmin(n1-1,j+1);
-                int jm  = fmax(0,j-1);
+                // int jp  = fmin(n1-1,j+1);
+                // int jm  = fmax(0,j-1);
                 int jmm = fmax(0,j-2);
-                vxx[i*n1+j] = 0.25*n1*n1* (phi[i*n1+jpp] - phi[i*n1+j] - phi[i*n1+j] + phi[i*n1+jmm]);
+                vxx[i*n1+j] = 0.25* (phi[i*n1+jpp] - phi[i*n1+j] - phi[i*n1+j] + phi[i*n1+jmm])/(dx*dx);
             }
         }
     }
 
-    void calculate_gradient_vyy(double* vyy, const double* phi){
+    void calculate_gradient_vyy(std::vector<double>& vyy, const double* phi, const double dx){
         for(int i=0;i<n2;++i){
             for(int j=0;j<n1;++j){
                 int ipp = fmin(n2-1,i+2);
                 // int ip  = fmin(n2-1,i+1);
                 // int im  = fmax(0,i-1);
                 int imm  = fmax(0,i-2);
-                vyy[i*n1+j] = 0.25*n2*n2* (phi[ipp*n1+j] - phi[i*n1+j] - phi[i*n1+j] + phi[imm*n1+j]);
+                vyy[i*n1+j] = 0.25 * (phi[ipp*n1+j] - phi[i*n1+j] - phi[i*n1+j] + phi[imm*n1+j])/(dx*dx);
             }
         }
     }
 
-    void calculate_gradient_vxy(double* vxy, const double* phi){
+    void calculate_gradient_vxy(std::vector<double>& vxy, const double* phi, const double dx){
         for(int i=0;i<n2;++i){
             for(int j=0;j<n1;++j){
                 int ip  = fmin(n2-1,i+1);
                 int im  = fmax(0,i-1);
                 int jp  = (int)fmin(n1-1,j+1);
                 int jm  = (int)fmax(0,j-1);
-                vxy[i*n1+j] = 0.25*n1*n2* (phi[ip*n1+jp] - phi[ip*n1+jm] - phi[im*n1+jp] +phi[im*n1+jm]);
+                vxy[i*n1+j] = 0.25 * (phi[ip*n1+jp] - phi[ip*n1+jm] - phi[im*n1+jp] +phi[im*n1+jm])/(dx*dx);
             }
         }
     }
@@ -110,13 +110,13 @@ class HelperClass{
      * (rhsx, rhsy) = \nabla (\phi - b)
      * (outputx, outputy) = g^{ij}\nabla (\phi - b)
      */
-    void compute_inverse_g(py::array_t<double>& outputx, py::array_t<double>& outputy, py::array_t<double>& phi_np, py::array_t<double>& psi_np, py::array_t<double>& rhsx_np, py::array_t<double>& rhsy_np){
+    void compute_inverse_g(py::array_t<double>& outputx_np, py::array_t<double>& outputy_np, py::array_t<double>& phi_np, py::array_t<double>& psi_np, py::array_t<double>& rhsx_np, py::array_t<double>& rhsy_np){
         py::buffer_info outputx_buf = outputx_np.request();
         py::buffer_info outputy_buf = outputy_np.request();
         py::buffer_info phi_buf = phi_np.request();
         py::buffer_info psi_buf = psi_np.request();
-        py::buffer_info rhsx_buf = rhs_np.request();
-        py::buffer_info rhsy_buf = rhs_np.request();
+        py::buffer_info rhsx_buf = rhsx_np.request();
+        py::buffer_info rhsy_buf = rhsy_np.request();
         double* outputx = static_cast<double *>(outputx_buf.ptr);
         double* outputy = static_cast<double *>(outputy_buf.ptr);
         double* phi = static_cast<double *>(phi_buf.ptr);
@@ -124,22 +124,20 @@ class HelperClass{
         double* rhsx = static_cast<double *>(rhsx_buf.ptr);
         double* rhsy = static_cast<double *>(rhsy_buf.ptr);
 
-        calculate_gradient_vxx(vxx_, psi);
-        calculate_gradient_vyy(vyy_, psi);
-        calculate_gradient_vxy(vxy_, psi);
+        calculate_gradient_vxx(vxx_, psi, dx_);
+        calculate_gradient_vyy(vyy_, psi, dx_);
+        calculate_gradient_vxy(vxy_, psi, dx_);
 
         // step 1: for each point we will find T(x)
         for(int i=0;i<n2;++i){
             for(int j=0;j<n1;++j){
-                double vxval=vx_[i*n1+j];
-                double vyval=vy_[i*n1+j];
 
-                double Sx1=(phi[i*n1+std::static_cast<int>(fmin(0,j+1))]-phi[i*n1+j])/dy;
-                double Sx2=(phi[std::static_cast<int>(fmin(0,i+1))*n1+j]-phi[i*n1+j])/dy;
+                double Sy1=(phi[i*n1+static_cast<int>(fmin(0,j+1))]-phi[i*n1+j])/dy_;
+                double Sy2=(phi[static_cast<int>(fmin(0,i+1))*n1+j]-phi[i*n1+j])/dy_;
 
-                double vxx_val = - interpolate_function(Sx1,Sx2,vxx_);
-                double vyy_val = - interpolate_function(Sx1,Sx2,vyy_);
-                double vxy_val = - interpolate_function(Sx1,Sx2,vxy_);
+                double vxx_val = - interpolate_function(Sy1,Sy2,vxx_);
+                double vyy_val = - interpolate_function(Sy1,Sy2,vyy_);
+                double vxy_val = - interpolate_function(Sy1,Sy2,vxy_);
 
                 outputx[i*n1+j] = vxx_val * rhsx[i*n1+j] + vxy_val * rhsy[i*n1+j];
                 outputy[i*n1+j] = vxy_val * rhsx[i*n1+j] + vyy_val * rhsy[i*n1+j];
@@ -182,5 +180,5 @@ PYBIND11_MODULE(screening, m) {
 
     py::class_<HelperClass>(m, "HelperClass")
             .def(py::init<py::array_t<double> &, double, double>()) // py::array_t<double>& phi_np, const double dx, const double dy
-            .def("compute_inverse_g", &HelperClass::compute_inverse_g)
+            .def("compute_inverse_g", &HelperClass::compute_inverse_g);
 }
